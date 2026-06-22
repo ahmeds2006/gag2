61f3c394f4f6308c6852668ea712c670d56c5a4e
A7med <114369976+ahmeds2006@users.noreply.github.com>
2026-06-22 16:56:22 +0300

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -4,27 +4,30 @@ TEMP_DOWNLOAD="/sdcard/Download/farm.json"
 DESTINATION="/storage/emulated/0/Delta/Workspace/LuminHub/settings/GameName/farm.json"
 
 echo "Downloading Config.. "
-curl -L -o "$TEMP_DOWNLOAD" "$URL"
 
+curl -L -o "$TEMP_DOWNLOAD" "$URL"
+sleep 1
+mkdir -p /storage/emulated/0/Delta/Workspace/LuminHub/settings/GameName
 echo "Config Set.. "
 mv "$TEMP_DOWNLOAD" "$DESTINATION"
 
+sleep 1
+NEW_FIL="/storage/emulated/0/Delta/Workspace/LuminHub/settings/GameName/autoload.txt"
 
-NEW_FILE="/storage/emulated/0/Delta/Workspace/LuminHub/settings/GameName/autoload.txt"
 
-
-echo "farm" > "$NEW_FILE"
+echo -n "farm" > "$NEW_FIL"
 
 echo "Auto load config done.. "
 
-
+sleep 1
 
 
 NEW_FILE="/storage/emulated/0/Delta/Autoexecute/lumin.txt"
+mkdir -p /storage/emulated/0/Delta/Autoexecute
 echo 'loadstring(game:HttpGet("https://rblxscripts.net/raw/lumin-hub-for-gag-2-auto-farm-harvesting-auto-buy-and-more-k-5c8bb5e7"))()' > "$NEW_FILE"
 
 echo "Auto execute ON.. "
-
+sleep 2
 
 
 
